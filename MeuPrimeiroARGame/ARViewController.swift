6229afb08c80f673e720586e5de53026cf58b375
Ricardo Venieris <rvenieris@gmail.com>
2023-02-04 18:58:59 -0300

    Todos os elementos desenhados e disponíveis. Pronto para preparar o código.

--- MeuPrimeiroARGame/ARViewController.swift
+++ MeuPrimeiroARGame/ARViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ARViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -23,7 +23,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/GameElements.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
